09a4a6aec5b7159e452e7e159b64850008dc4557
Axemasta <alexduffell94@gmail.com>
2019-10-28 22:40:19 +0000

    Refactored js, empty characters are now indicated
    Refactored the js to be alot dryer, now the ui updates based on password changes & value changes. Added unselected state for characters out of range. Added edge case fix for space characters.

--- js/app.js
+++ js/app.js
@@ -1,63 +1,49 @@
 $(document).ready(function () {
 
+    var _password = "" 
+
     $("#passwordEntry").on("input", function () {
+        _password = $("#passwordEntry").val();
+        console.log(_password);
 
-        // var textValue = $("#passwordEntry").val();
-
-        // RenderPassword(textValue);
+        RenderPassword($("#display-cell1-input"), $("#display-cell1-value"));
+        RenderPassword($("#display-cell2-input"), $("#display-cell2-value"));
+        RenderPassword($("#display-cell3-input"), $("#display-cell3-value"));
     });
 
-    function RenderPassword(password) {
-        console.log(password);
+    function RenderPassword(input, label) {
+        var numberValue = input.val();
 
-        $("#rendered-password-table tbody > tr").empty();
+        numberValue = parseInt(numberValue);
+        numberValue = numberValue - 1;
 
-        var i;
+        if (numberValue >= _password.length) {
+            label.text("?");
+            
+            label.addClass("unselected-character"); 
+            return;
+        } else {
+            label.removeClass("unselected-character");
+        }
 
-        for (i = 0; i < password.length; i++) {
+        var character = _password[numberValue];
 
-            var character = password[i];
-
-            $("#rendered-password-table").append("<tr><td>" + character + "</td><td>" + (i + 1) + "</td></tr>");
+        if (character == " ") {
+            label.html("&nbsp;");
+        } else {
+            label.text(character);
         }
     }
 
     $("#display-cell1-input").on("input", function () {
-
-        var numberValue = $("#display-cell1-input").val();
-        var textValue = $("#passwordEntry").val();
-
-        numberValue = parseInt(numberValue);
-        numberValue = numberValue - 1;
-
-        var character = textValue[numberValue];
-
-        $("#display-cell1-value").text(character);
+        RenderPassword($("#display-cell1-input"), $("#display-cell1-value"));
     });
 
     $("#display-cell2-input").on("input", function () {
-
-        var numberValue = $("#display-cell2-input").val();
-        var textValue = $("#passwordEntry").val();
-
-        numberValue = parseInt(numberValue);
-        numberValue = numberValue - 1;
-
-        var character = textValue[numberValue];
-
-        $("#display-cell2-value").text(character);
+        RenderPassword($("#display-cell2-input"), $("#display-cell2-value"));
     });
 
     $("#display-cell3-input").on("input", function () {
-
-        var numberValue = $("#display-cell3-input").val();
-        var textValue = $("#passwordEntry").val();
-
-        numberValue = parseInt(numberValue);
-        numberValue = numberValue - 1;
-
-        var character = textValue[numberValue];
-
-        $("#display-cell3-value").text(character);
+        RenderPassword($("#display-cell3-input"), $("#display-cell3-value"));
     });
 });
